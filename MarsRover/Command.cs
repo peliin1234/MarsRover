5cfe7ec6c9db133b972ce94ceeef3b3071d89212
peloo <peliintas42@gmail.com>
2022-11-30 17:02:22 +0300

    oluşturuldu

---
+++ MarsRover/Command.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Command
+    {
+        Point position;
+        Directions direction;
+        Point boardSize;
+        public Command(int x, int y, Directions direction, int maxBoardX, int maxBoardY)
+        {
+            maxBoardX = maxBoardX > 0 ? maxBoardX : 1;
+            maxBoardY = maxBoardY > 0 ? maxBoardY : 1;
+            x = (x < 0 || x > maxBoardX) ? 0 : x;
+            y = (y < 0 || y > maxBoardY) ? 0 : y;
+
+            boardSize = new Point(maxBoardX, maxBoardY);
+            position = new Point(x, y);
+            this.direction = direction;
+        }
+        private void RotateLeft()
+        {
+            switch (this.direction)
+            {
+                case Directions.N:
+                    this.direction = Directions.W;
+                    break;
+                case Directions.S:
+                    this.direction = Directions.E;
+                    break;
+                case Directions.E:
+                    this.direction = Directions.N;
+                    break;
+                case Directions.W:
+                    this.direction = Directions.S;
+                    break;
+            }
+        }
+        private void RotateRight()
+        {
+            switch (this.direction)
+            {
+                case Directions.N:
+                    this.direction = Directions.E;
+                    break;
+                case Directions.S:
+                    this.direction = Directions.W;
+                    break;
+                case Directions.E:
+                    this.direction = Directions.S;
+                    break;
+                case Directions.W:
+                    this.direction = Directions.N;
+                    break;
+            }
+        }
+        private bool Move()
+        {
+            switch (this.direction)
+            {
+                case Directions.N:
+                    this.position.Y += 1;
+                    break;
+                case Directions.S:
+                    this.position.Y -= 1;
+                    break;
+                case Directions.E:
+                    this.position.X += 1;
+                    break;
+                case Directions.W:
+                    this.position.X -= 1;
+                    break;
+            }
+
+            if (position.X > boardSize.X || position.Y > boardSize.Y || position.X < 0 || position.Y < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public string ApplyCommands(string commands)
+        {
+            int commandCount = 0;
+            foreach (var command in commands)
+            {
+                switch (command)
+                {
+                    case 'L':
+                        RotateLeft();
+                        break;
+                    case 'R':
+                        RotateRight();
+                        break;
+                    case 'M':
+                        if (!Move())
+                            return $"The rover couldnt execute the move command with index : {commandCount} (Rovers new position is : {position.X} {position.Y} but the board is : {boardSize.X} X {boardSize.Y})";
+                        break;
+                    default:
+                        return $"The rover doesn't know the command with index {commandCount} Which is : {command}";
+                }
+                commandCount++;
+            }
+            return $"{position.X} {position.Y} {direction}";
+        }
+    }
+}
--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MarsRover;
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Please enter upper-right coordinates of the plateau. Example (5 5):");
+        string boardSize = Console.ReadLine().Trim();
+
+        List<int> XY = boardSize.Split(' ').ToList().Select(x => Convert.ToInt32(x)).ToList();
+
+        if (XY.Count != 2 || XY.Any(x => x < 0))
+        {
+            //Rover class also checks validity
+            throw new Exception("Invalid board size.");
+        }
+
+        while (true)
+        {
+            try
+            {
+
+                Console.WriteLine("Please enter plateau exploration information. Example (1 2 N):");
+                string text = Console.ReadLine().ToUpper();
+                List<string> roverStartingStats = text.Trim().Split(' ').ToList();
+
+                if (roverStartingStats.Count != 3)
+                {
+                    //Rover class also checks validity
+                    throw new Exception("Invalid rover input.");
+                }
+                int startingX = Convert.ToInt32(roverStartingStats[0]);
+                int startingY = Convert.ToInt32(roverStartingStats[1]);
+
+                Directions direction = (Directions)Enum.Parse(typeof(Directions), roverStartingStats[2]);
+
+                Console.WriteLine("Please enter comands.Only L, M, R routing commands must be used. Example (LMLMLMLMM):");
+                string roverCommands = Console.ReadLine();
+
+                Command rover = new Command(startingX, startingY, direction, XY[0], XY[1]);
+
+                Console.WriteLine(rover.ApplyCommands(roverCommands));
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
